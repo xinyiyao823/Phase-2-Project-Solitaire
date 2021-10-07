a58600a720f7979a07b3feff9aafee886b85ee57
Matthew Blank <matthew.3lank@gmail.com>
2021-10-07 01:38:40 -0400

    minor change to render cards with '10' instead of '0'

--- solitaire/src/components/Card.js
+++ solitaire/src/components/Card.js
@@ -20,61 +20,7 @@ function Card({ card, row, col, board, setBoard, moveCard, canMoveCard, selected
     (card.value[1] === "♥" || card.value[1] === "♦")? "red" : "black"
     : "hidden"
     const cardColor = color()
-    const selectionHighlight = isSelected? " selected" : ""
-
-
-
-    const determineArray = (checkCard) => {
-        const checkArrays = ['columns', 'aces', 'deck', 'draw']
-
-        let returnVal = []
-
-        checkArrays.forEach( key => {
-            board[key].forEach( (array, i) => {
-                if (array[0]) {
-                    if (array.some( card => card.value === checkCard.value)) {
-                        console.log("found matching key: ", key)
-                        const indexA = i
-                        const indexB = array.findIndex( card => card.value === checkCard.value)
-                        console.log("ARRAY: ", array)
-                        console.log("INDEXA: ", indexA)
-                        console.log("INDEXB: ", indexB)
-                        console.log("ARRAYINDEXA: ", array[indexA])
-
-                        // const indexB = array[indexA].findIndex( card => card.value === checkCard.value)
-                        // console.log("INDEXB: ", indexB)
-
-                        returnVal = [ key, indexA, indexB ]
-
-                        
-                    } else {
-                        console.log("not a match")
-                    }
-                } else {
-                    console.log("false")
-                }
-
-            })
-        } )
-
-        return returnVal
-    }
-
-
-
-            // console.log("Checking array: ", array)
-            // console.log(board[array])
-
-
-        //     if (board[array].some( nestedArray => nestedArray.some(
-        //         card => card.value === checkCard.value )
-        //     )) {
-        //         console.log("found "+checkCard+" in "+array)
-        //     } else {
-        //         console.log("not found")
-        //     }
-        // } )
-    
+    const selectionHighlight = isSelected? " selected" : ""    
 
     // handle click event
     const clickHandler = (e) => {
@@ -99,7 +45,7 @@ function Card({ card, row, col, board, setBoard, moveCard, canMoveCard, selected
 
     return (
          <div className={"card " + cardColor + selectionHighlight} onClick={clickHandler}>
-             <p>{card.show? `${card.value[0]} ${card.value[1]}` : ""}</p>
+             <p>{card.show? `${card.value[0] === "0"? "10" : card.value[0]} ${card.value[1]}` : ""}</p>
          </div>
     )
 }
